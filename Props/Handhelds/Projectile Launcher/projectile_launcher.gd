eb7d56f3c9235f6b9fa685ac32a9db5c7e633ff5
HarryLBonGithub <111366765+HarryLBonGithub@users.noreply.github.com>
2023-09-19 22:43:42 -0400

    add projectile launcher
    - created a generic projectile launcher that spawns a projectile
    - still needs fire rate and ammo considerations added in

---
+++ Props/Handhelds/Projectile Launcher/projectile_launcher.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var projectile : PackedScene
+@export var maxAmmo = 10
+@export var currentAmmo = 10
+
+@onready var room = get_tree().current_scene
+@onready var barrelNode = $Barrel
+
+var active = true
+
+func _process(delta):
+	
+	if Input.is_action_just_pressed("use_tool"):	
+		if maxAmmo > 0 and currentAmmo <=0:
+			dryFire()
+		else:
+			var proj = projectile.instantiate()
+			proj.transform = barrelNode.global_transform
+			room.add_child(proj)
+
+func dryFire():
+	pass
