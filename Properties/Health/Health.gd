bb6c4516484cb98d004a2138b0d12fb382a31cdc
HarryLBonGithub <111366765+HarryLBonGithub@users.noreply.github.com>
2023-09-24 17:20:23 -0400

    add healthbar functionality
    - the health property now takes a progress bar node as a variable
    - updates the bar on ready and when damage is taken

--- Properties/Health/Health.gd
+++ Properties/Health/Health.gd
@@ -7,8 +7,18 @@ signal healed
 @export var maxHealth = 30
 @export var currentHealth = 30
 
+@export var healthUI : ProgressBar
+
+func _ready():
+	if healthUI:
+		healthUI.max_value = maxHealth
+		healthUI.value = currentHealth
+
 func take_damage(damage):
 	currentHealth -= damage
+	
+	if healthUI:
+		healthUI.value = currentHealth
 	
 	if currentHealth <= 0:
 		var parent = get_parent()
