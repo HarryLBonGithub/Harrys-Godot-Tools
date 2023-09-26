418d28aae2c1e78196ca5260ad0be1268ad61491
HarryLBonGithub <111366765+HarryLBonGithub@users.noreply.github.com>
2023-09-26 19:35:28 -0400

    add basic hand organizer
    - created a "hand" node used to organize the tools that the player has available
    - the hand sets an active tool and makes it active and visible
    - still needs to be able to cycle

--- Organizers/Hand/hand.gd
+++ Organizers/Hand/hand.gd
@@ -2,10 +2,35 @@ extends Node3D
 
 var belt = []
 
-var activeTool
+@onready var activeTool = 0
 
-@export var ammoUI : PackedScene
+@export var ammoUI : AmmoTracker
 
 func _ready():
-	pass
+	
+	belt = get_children()
+	
+	for item in belt:
+		item.active = false
+		item.visible = false
+	
+	belt[activeTool].active = true
+	belt[activeTool].visible = true
+	
+	if ammoUI:
+		ammoUI.toolName.text = str(belt[activeTool].toolName)
+		ammoUI.max.text = str(belt[activeTool].maxAmmo)
+		ammoUI.current.text = str(belt[activeTool].currentAmmo)
+
+func _process(delta):
+	updateUI()
+
+
+func updateUI():
+	if !ammoUI:
+		return
+	
+	if Input.is_action_pressed("use_tool"):
+		ammoUI.max.text = str(belt[activeTool].maxAmmo)
+		ammoUI.current.text = str(belt[activeTool].currentAmmo)
 	
