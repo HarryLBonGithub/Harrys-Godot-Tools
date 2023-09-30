944562172398f67a65c45bb0634872577349c070
HarryLBonGithub <111366765+HarryLBonGithub@users.noreply.github.com>
2023-09-29 20:39:26 -0400

    refactor tool to 'tool-frame'
    - tools can now be built on a 'tool-frame' node
    - separated the projectile launching action into its own node, so it can now be added to things other than a user held tool, like a turret
    - now, a hitscan tool can be added to the tool frame to make a tool with different functionality, but can still be easily edited

--- Props/Handhelds/Projectile Launcher/projectile_launcher.gd
+++ Props/Handhelds/Projectile Launcher/projectile_launcher.gd
@@ -1,71 +1,17 @@
 extends Node3D
 
-@export var toolName = "Launcher"
+class_name ActionTool
 
 @export var projectile : PackedScene
-@export var ammoNode : Ammo
-@export var ammoUI : AmmoTracker
-@export var fireRate : Timer
 
 @export var active = true
 
 @onready var room = get_tree().current_scene
-@onready var barrelNode = $Barrel
 
-var readyToFire = true
 
-func _ready():
-	
-	if !active:
-		return
-	
-	if ammoUI:
-		ammoUI.toolName.text = str(toolName)
-		if ammoNode:
-			ammoUI.max.text = str(ammoNode.maxAmmo)
-			ammoUI.current.text = str(ammoNode.currentAmmo)
-
-func _process(delta):
-	fire()
-	ready()
-	
-
-func dryFire():
-	pass
-
-func fire():
-	if !active or !readyToFire:
-		return
-
-	
-	if Input.is_action_pressed("use_tool"):	
-		if ammoNode and ammoNode.currentAmmo <=0:
-			dryFire()
-		else:
-			var proj = projectile.instantiate()
-			proj.transform = barrelNode.global_transform
-			room.add_child(proj)
-			readyToFire = false
-			
-			if fireRate:
-				fireRate.start()
-			
-			if !ammoNode:
-				return
-			ammoNode.currentAmmo -= 1
-			
-			if !ammoUI:
-				return
-			ammoUI.current.text = str(ammoNode.currentAmmo)
-
-func ready():
-	if Input.is_action_just_released("use_tool"):
-		readyToFire = true
-	
-	if !fireRate:
-		return
-	
-	if fireRate.is_stopped():
-		readyToFire = true
+func use():
+	var proj = projectile.instantiate()
+	proj.transform = self.global_transform
+	room.add_child(proj)
 	
 	
---
+++ Props/Handhelds/Tool Frame/tool_frame.gd
@@ -0,0 +1,63 @@
+extends Node3D
+
+@export var toolName = "Tool Frame"
+
+@export var actionTool : ActionTool
+@export var ammoNode : Ammo
+@export var ammoUI : AmmoTracker
+@export var fireRate : Timer
+
+@export var active = true
+
+var readyToFire = true
+
+func _ready():
+	if not active:
+		return
+	
+	if ammoUI:
+		ammoUI.toolName.text = str(toolName)
+		if ammoNode:
+			ammoUI.max.text = str(ammoNode.maxAmmo)
+			ammoUI.current.text = str(ammoNode.currentAmmo)
+
+func _process(delta):
+	if not actionTool or not active:
+		return
+	useTool()
+	readyTool()
+
+func useTool():
+	if Input.is_action_pressed("use_tool"):
+		
+		if not readyToFire:
+			return
+		
+		if ammoNode:
+			if ammoNode.currentAmmo <=0:
+				dryFire()
+				return
+			else:
+				ammoNode.currentAmmo -= 1
+		
+		if Input.is_action_pressed("use_tool"):
+			actionTool.use()
+			readyToFire = false
+		
+		if fireRate:
+			fireRate.one_shot = true
+			fireRate.start()
+			
+
+func readyTool():
+	if Input.is_action_just_released("use_tool"):
+		readyToFire = true
+	
+	if !fireRate:
+		return
+	
+	if fireRate.is_stopped():
+		readyToFire = true
+
+func dryFire():
+	return
