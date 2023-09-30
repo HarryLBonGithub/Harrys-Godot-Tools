2387aba97e0e4eef0ee39ce16df0a51986a17e5b
HarryLBonGithub <111366765+HarryLBonGithub@users.noreply.github.com>
2023-09-30 13:52:23 -0400

    adjust health
    - simple health pickup now only triggers if someone picking it up is not at max health

--- Building Tools/Properties/Damage and Health Box/healing_box.gd
+++ Building Tools/Properties/Damage and Health Box/healing_box.gd
@@ -6,13 +6,16 @@ signal healthDelivered
 func _on_area_entered(area):
 	if area.has_node("Health"):
 		var target = area.find_child("Health")
-		target.heal_damage(healing)
 		
-		emit_signal("healthDelivered")
+		if target.currentHealth < target.maxHealth:
+			target.heal_damage(healing)
+			emit_signal("healthDelivered")
 
 
 func _on_body_entered(body):
 	if body.has_node("Health"):
 		var target = body.find_child("Health")
-		target.heal_damage(healing)
-		emit_signal("healthDelivered")
+		
+		if target.currentHealth < target.maxHealth:
+			target.heal_damage(healing)
+			emit_signal("healthDelivered")
